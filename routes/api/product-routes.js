10effc028d976cdbea4b128d6df009b5906d862d
kristyyip <kristyyip51998@gmail.com>
2024-07-27 14:44:10 -0400

    added tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -120,7 +120,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
@@ -129,7 +129,7 @@ router.delete('/:id', (req, res) => {
 
     if (!productData) {
       res.status(404).json({ message: "No product found with this id!"});
-      return
+      return;
     }
 
     res.status().json(productData);
